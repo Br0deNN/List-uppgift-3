026e7e6fd1ebc29e88564e3388e9e21646705e6e
7R1XY <theo.broden@gmail.com>
2024-02-02 12:59:17 +0100

    Add project files.

---
+++ List uppgift 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> länder = new Dictionary<string, int>();
+        länder["Svergie"] = 450;
+        länder["Norge"] = 385;
+        länder["Danmark"] = 43;
+        länder["Finland"] = 338;
+
+        Console.WriteLine("Skriv in Svergie, Norge, Danmark eller finland för att få reda på dess area");
+        string input = Console.ReadLine();
+
+        Console.WriteLine($"{input}s area är {länder[input]}-tusen kvadratkilometer");
+            
+    }
+}
